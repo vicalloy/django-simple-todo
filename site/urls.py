b2320397baf39582d0467990cc761b5cefcdd082
vicalloy <zbirder@gmail.com>
2011-07-29 13:52:11 +0800

    * move all code to site/

--- site/urls.py
+++ site/urls.py
@@ -15,16 +15,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
 
 urlpatterns += staticfiles_urlpatterns()
-
-from simpletodo import views
-
-urlpatterns += patterns('',
-    url('^$', views.index, name='todo_idx'),
-    url('^todo/new/$', views.new, name='todo_new'),
-    url('^todo/(?P<id>\d+)/edit/$', views.edit, name='todo_edit'),
-    url('^todo/(?P<id>\d+)/delete/$', views.delete, name='todo_delete'),
-    url('^todo/(?P<id>\d+)/finish/$', views.finish, name='todo_finish'),
-)
